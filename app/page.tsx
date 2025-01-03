f1ae816fc073ad30844392918b6fceffcbda59dd
DreiDev04 <tacujan.andrei@gmail.com>
2025-01-03 21:59:18 +0800

    added: nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 font-bold text-[8rem]">
-      Putangina mo Gilbert
+    <div className="h-screen sm:grid sm:grid-cols-12">
+      <div className="sm:col-span-3 bg-red-500 h-full">as</div>
+      <div className="sm:col-span-9 bg-blue-500 h-full">as</div>
     </div>
   );
 }
