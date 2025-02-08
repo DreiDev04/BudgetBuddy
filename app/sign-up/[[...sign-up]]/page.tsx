1de0c626b2384392c7327ff133dd4650e3f8980b
DreiDev04 <tacujan.andrei@gmail.com>
2025-02-08 17:08:42 +0800

    added: signup redirect

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -2,16 +2,19 @@ import { SignUp } from "@clerk/nextjs";
 
 const appearanceVariables = {
   variables: {
-    colorPrimary: 'hsl(142.1, 76.2%, 36.3%)',
-    colorText: 'hsl(240, 10%, 3.9%)',
-    colorBackground: 'hsl(0, 0%, 90%)',
+    colorPrimary: "hsl(142.1, 76.2%, 36.3%)",
+    colorText: "hsl(240, 10%, 3.9%)",
+    colorBackground: "hsl(0, 0%, 90%)",
   },
 };
 
 export default function Page() {
   return (
     <div className="min-h-screen bg-background flex flex-col items-center justify-center">
-      <SignUp appearance={appearanceVariables} />
+      <SignUp
+        appearance={appearanceVariables}
+        forceRedirectUrl={"/api/onboardingCheck"}
+      />
     </div>
   );
 }
